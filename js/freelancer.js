9a77eef1965047896b1f1258b64f77ca63a99d8b
Yaniv Duke <yaniv.duke@gmail.com>
2018-10-25 01:11:58 +0300

    describtion

--- js/freelancer.js
+++ js/freelancer.js
@@ -50,13 +50,13 @@
   $(window).scroll(navbarCollapse);
 
   // Modal popup$(function () {
-  $('.portfolio-item').magnificPopup({
+  $('.services-item').magnificPopup({
     type: 'inline',
     preloader: false,
     focus: '#username',
     modal: true
   });
-  $(document).on('click', '.portfolio-modal-dismiss', function(e) {
+  $(document).on('click', '.services-modal-dismiss', function(e) {
     e.preventDefault();
     $.magnificPopup.close();
   });
