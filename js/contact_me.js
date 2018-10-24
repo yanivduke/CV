fce14ad9ffb4c45662064cb6b55495ef0c0ed87e
Yaniv Duke <yaniv.duke@gmail.com>
2018-10-24 10:04:21 +0300

    send email via rest via gmail

--- js/contact_me.js
+++ js/contact_me.js
@@ -13,19 +13,20 @@ $(function() {
       var message = $("textarea#message").val();
       var firstName = name; // For Success/Failure Message
       // Check for white space in name for Success/Fail message
+      
       if (firstName.indexOf(' ') >= 0) {
         firstName = name.split(' ').slice(0, -1).join(' ');
       }
+      var body = JSON.stringify({name: name, email: email, message: message})
+      //alert(body)
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
         url: "https://dev.dukecode.com/api/sendMail",
         type: "POST",
-        data: {
-          name: name,
-          email: email,
-          message: message
-        },
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        data: body,
         cache: false,
         success: function() {
           // Success message
@@ -33,7 +34,7 @@ $(function() {
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
           $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
+            .append("<strong>פניתך נשלחה בהצלחה, נחזור בהקדם האפשרי. </strong>");
           $('#success > .alert-success')
             .append('</div>');
           //clear all fields
@@ -44,7 +45,7 @@ $(function() {
           $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+          $('#success > .alert-danger').append($("<strong>").text("מצטערים " + firstName + ", נראה שהפנייה לא נשלחה, שלח את פנייתך לכתובת yaniv.duke@gmail.com ואחזור בהקדם האפשרי."));
           $('#success > .alert-danger').append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
